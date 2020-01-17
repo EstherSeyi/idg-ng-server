0eee12edbe5ecf9d9851c9466a1cee564859bcfe
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-01-17 15:58:31 +0100

    Controller to get a user by ID

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,13 @@
 import User from '../models/user';
 
+export async function getAUser(userID: string) {
+  return User.findOne({ id: userID }).select({
+    _id: 0,
+    __v: 0,
+    password: 0,
+  });
+}
+
 export async function getAUserByEmail(email: string) {
   return User.findOne({ email }).select({
     _id: 0,
