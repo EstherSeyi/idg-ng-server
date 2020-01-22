475ce9f2e9c59f53261cb6341bf04b9364f0969e
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-01-22 16:04:33 +0100

    Expose endpoint to filter candidates

--- src/routes/candidates.ts
+++ src/routes/candidates.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { fetchCandidates } from '../controllers/candidates';
+import { fetchCandidates, filterCandidates } from '../controllers/candidates';
 
 const router = Router();
 
 router.get('/', fetchCandidates);
+router.get('/filter', filterCandidates);
 
 export default router;
--- src/services/candidates.ts
+++ src/services/candidates.ts
@@ -1,7 +1,7 @@
 import Candidate from '../models/candidate';
 
-export async function getCandidates(pageSize: number, pageNum: number) {
-  pageSize = pageSize || 7;
+export async function getCandidates(pageNum: number) {
+  const pageSize = 7;
   pageNum = pageNum || 1;
 
   //  Calculate number of documents to skip
@@ -9,7 +9,23 @@ export async function getCandidates(pageSize: number, pageNum: number) {
 
   const candidates = await Candidate.find({})
     .skip(skips)
-    .limit(pageSize);
+    .limit(pageSize)
+    .select({ _id: 0, __v: 0 });
+
+  return candidates;
+}
+
+export async function selectCandidates(pageNum: number, filterParams: {}) {
+  pageNum = pageNum || 1;
+  const pageSize = 7;
+
+  //  Calculate number of documents to skip
+  const skips = pageSize * (pageNum - 1);
+
+  const candidates = await Candidate.find(filterParams)
+    .skip(skips)
+    .limit(pageSize)
+    .select({ _id: 0, __v: 0 });
 
   return candidates;
 }
