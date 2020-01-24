18c153218375eefd8304a2abd680582cb7490038
EstherSeyi <sheyiogundijo@gmail.com>
2020-01-24 14:54:35 +0100

    modifies cors

--- src/app.ts
+++ src/app.ts
@@ -50,7 +50,7 @@ app.use(
 
 app.disable('x-powered-by');
 app.use(compression());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
